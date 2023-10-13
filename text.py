6fce90a7563ad03780a217b3d939dca621a784d0
ngjunsiang <45561895+ngjunsiang@users.noreply.github.com>
2023-10-13 23:55:25 +0000

    Add inline option to text.prompt_valid_choice()

--- text.py
+++ text.py
@@ -7,18 +7,24 @@ from typing import Any
 
 def prompt_valid_choice(
     options: list[str],
+    *,  # parameters below are keyword-only
+    inline: bool = False,
     cancel: bool = False,
     prompt: str = "Pick an option",
     errmsg: str = "Invalid option, try again.",
     prelude: str = "",
-) -> str:
-    if cancel:
-        prompt += " (or 'b' to go back)"
-    prompt += ": "
+) -> "str | None":
+    # Prompt preparation
+    if inline and cancel:
+        prompt += f" ({'/'.join(options)} or 'b' to go back): "
+    elif not inline and cancel:
+        prompt += " (or 'b' to go back): "
+
     if prelude:
         print(prelude)
-    for i, option in enumerate(options, start=1):
-        print(f"{i}. {option}")
+    if not inline:
+        for i, option in enumerate(options, start=1):
+            print(f"{i}. {option}")
     print('')
     choice = input(prompt)
     while not (choice.isdecimal() and 1 <= int(choice) <= len(options)):
